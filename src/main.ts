808419dc82f4c86f7fcf41613030b80a85b40d01
lyvathanak <ngovxnak@gmail.com>
2025-06-06 09:55:14 +0700

    Initial commit on backend branch

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
+  // Add this line to enable CORS
   app.enableCors();
 
   await app.listen(3000);
