2cd8b6e532e566000260135442f12fbd1533cfe0
lyvathanak <ngovxnak@gmail.com>
2025-06-23 00:12:13 +0700

    Final

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  publicPath: '/',
   transpileDependencies: true,
 
   // Add this section to define feature flags
