4795ff77dfffc8a8adc96f8b3a71205692be0c04
lyvathanak <ngovxnak@gmail.com>
2025-06-25 16:20:15 +0700

    fix: Listen on 0.0.0.0 for render

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,8 @@ async function bootstrap() {
     transform: true 
   }));
 
-  await app.listen(5000, '0.0.0.0'); 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+await app.listen(5000, '0.0.0.0'); 
+
+console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
